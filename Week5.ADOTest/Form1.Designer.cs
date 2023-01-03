4254cf2fe1fbd696a742abf208ea7a41ae1550de
Abdul Aziz <aabdul9700@gmail.com>
2023-01-03 17:40:23 +0530

    Add project files.

---
+++ Week5.ADOTest/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Week5.ADOTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddCard = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtCardNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNameOnCard = new System.Windows.Forms.TextBox();
+            this.txtCVVNumber = new System.Windows.Forms.TextBox();
+            this.txtExpiryDate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCardType = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBalance = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAddCard
+            // 
+            this.btnAddCard.AutoSize = true;
+            this.btnAddCard.Location = new System.Drawing.Point(390, 392);
+            this.btnAddCard.Name = "btnAddCard";
+            this.btnAddCard.Size = new System.Drawing.Size(82, 30);
+            this.btnAddCard.TabIndex = 0;
+            this.btnAddCard.Text = "AddCard";
+            this.btnAddCard.UseVisualStyleBackColor = true;
+            this.btnAddCard.Click += new System.EventHandler(this.btnAddCard_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(599, 402);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(51, 20);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "label1";
+            // 
+            // txtCardNumber
+            // 
+            this.txtCardNumber.Location = new System.Drawing.Point(390, 77);
+            this.txtCardNumber.Name = "txtCardNumber";
+            this.txtCardNumber.Size = new System.Drawing.Size(121, 26);
+            this.txtCardNumber.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Card Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name On Card";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CVVNumber";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Expiry Date";
+            // 
+            // txtNameOnCard
+            // 
+            this.txtNameOnCard.Location = new System.Drawing.Point(390, 133);
+            this.txtNameOnCard.Name = "txtNameOnCard";
+            this.txtNameOnCard.Size = new System.Drawing.Size(121, 26);
+            this.txtNameOnCard.TabIndex = 7;
+            this.txtNameOnCard.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtCVVNumber
+            // 
+            this.txtCVVNumber.Location = new System.Drawing.Point(390, 183);
+            this.txtCVVNumber.Name = "txtCVVNumber";
+            this.txtCVVNumber.Size = new System.Drawing.Size(121, 26);
+            this.txtCVVNumber.TabIndex = 8;
+            // 
+            // txtExpiryDate
+            // 
+            this.txtExpiryDate.Location = new System.Drawing.Point(390, 234);
+            this.txtExpiryDate.Name = "txtExpiryDate";
+            this.txtExpiryDate.Size = new System.Drawing.Size(121, 26);
+            this.txtExpiryDate.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(273, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Card Type";
+            // 
+            // txtCardType
+            // 
+            this.txtCardType.Location = new System.Drawing.Point(390, 285);
+            this.txtCardType.Name = "txtCardType";
+            this.txtCardType.Size = new System.Drawing.Size(121, 26);
+            this.txtCardType.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(273, 333);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Balance";
+            // 
+            // txtBalance
+            // 
+            this.txtBalance.Location = new System.Drawing.Point(390, 333);
+            this.txtBalance.Name = "txtBalance";
+            this.txtBalance.Size = new System.Drawing.Size(121, 26);
+            this.txtBalance.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBalance);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCardType);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtExpiryDate);
+            this.Controls.Add(this.txtCVVNumber);
+            this.Controls.Add(this.txtNameOnCard);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCardNumber);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnAddCard);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddCard;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtCardNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNameOnCard;
+        private System.Windows.Forms.TextBox txtCVVNumber;
+        private System.Windows.Forms.TextBox txtExpiryDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCardType;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBalance;
+    }
+}
+
---
+++ Week5.ADOTest/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Week5.ADOTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddCard_Click(object sender, EventArgs e)
+        {
+            string cardNumber = txtCardNumber.Text;
+            string nameOnCard = txtNameOnCard.Text;
+            string cardType = txtCardType.Text;
+            string cvvNumber = txtCVVNumber.Text;
+            DateTime expiryDate = Convert.ToDateTime(txtExpiryDate.Text);
+            decimal balance = Convert.ToDecimal(txtBalance.Text);
+
+            // Create a connection to the database
+            using (SqlConnection conObj = new SqlConnection(ConfigurationManager.ConnectionStrings["QuickKartCon"].ConnectionString))
+            {
+                conObj.Open();
+                SqlCommand cmdObj = new SqlCommand("usp_CreateNewCardDetails", conObj);
+                cmdObj.CommandType = CommandType.StoredProcedure;
+
+                // input parameters
+                cmdObj.Parameters.AddWithValue("@CardNumber", cardNumber);
+                cmdObj.Parameters.AddWithValue("@NameOnCard", nameOnCard);
+                cmdObj.Parameters.AddWithValue("@CardType", cardType);
+                cmdObj.Parameters.AddWithValue("@CVVNumber", cvvNumber);
+                cmdObj.Parameters.AddWithValue("@ExpiryDate", expiryDate);
+                cmdObj.Parameters.AddWithValue("@Balance", balance);
+
+                // output parameter
+                SqlParameter reslt = new SqlParameter("@Result", SqlDbType.Int);
+                reslt.Direction = ParameterDirection.Output;
+                cmdObj.Parameters.Add(reslt);
+
+                // Execute procedure
+                cmdObj.ExecuteNonQuery();
+
+                int result = (int)reslt.Value;
+
+                if (result >0)
+                {
+                    lblResult.Text = "Card details added successfully. Card number: " + cardNumber;
+                }
+                else
+                {
+                    lblResult.Text = "Error adding card details. Error code: " + result;
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
